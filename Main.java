98155ab3aad544d159b36f465fdebe24ec06d93a
ZalinaZaseeva <>
2022-12-11 18:24:44 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(final String[] args) {
+    String name = "Zalina ";
+    String surname = "Zaseeva";
+    int income = 120000;
+    int spending = 80000;
+    int surplus = income-spending;
+    System.out.println(name + surname + ", surplus: -" + surplus + " руб.");
+  }
+
+}
